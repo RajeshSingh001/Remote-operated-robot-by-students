5499c15f1c90e4a20eea5d3ae6c00b7ba4d3e421
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-30 10:24:48 +0530

    Code TX and and RX, model

---
+++ receiver_task1/receiver_task1.ino
@@ -0,0 +1,90 @@
+
+ #include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int x = 10;
+int y = 9;
+int z = 8;
+int w = 7;
+int M16 = 6;
+int M21 = 13;
+
+void setup() 
+{
+  lcd.begin(16,2);
+  // initialize serial:
+  Serial.begin(9600);
+ pinMode(x, OUTPUT); 
+ pinMode(y, OUTPUT);
+ pinMode(z, OUTPUT);
+ pinMode(w, OUTPUT);
+ pinMode(M16, OUTPUT);
+ pinMode(M21, OUTPUT);
+}
+
+void loop() 
+{
+  // print the string when a newline arrives:
+  int fsr1 = Serial.read();
+  if (fsr1 == 1)
+  {
+
+    lcd.setCursor(0,0);
+    lcd.print("   FORWARD");
+    digitalWrite(M16, HIGH);
+    digitalWrite(M21, HIGH);
+    digitalWrite(x, HIGH);
+    digitalWrite(y, LOW);
+    digitalWrite(z, HIGH);
+    digitalWrite(w, LOW);
+    
+  }
+    else if(fsr1 == 2)
+    {
+    
+      lcd.setCursor(0,0);
+      lcd.print("  BACKWARD");
+    digitalWrite(M16, HIGH);
+  digitalWrite(M21, HIGH);  
+    digitalWrite(x, LOW);
+    digitalWrite(y, HIGH);
+    digitalWrite(z, LOW);
+    digitalWrite(w, HIGH); 
+    }
+ else if(fsr1 == 3)
+    {
+     
+      lcd.setCursor(0,0);
+      lcd.print("RIGHT TURN"); 
+      digitalWrite(M16, HIGH);
+      digitalWrite(M21, LOW);
+    digitalWrite(x, HIGH);
+    digitalWrite(y, LOW);
+    digitalWrite(z, LOW);
+    digitalWrite(w, LOW);  
+    }
+ else if(fsr1 == 4)
+    {
+      
+      lcd.setCursor(0,0);
+      lcd.print("LEFT TURN");
+      digitalWrite(M16, LOW);
+      digitalWrite(M21, HIGH);
+    digitalWrite(x, LOW);
+    digitalWrite(y, LOW);
+    digitalWrite(z, HIGH);
+    digitalWrite(w, LOW);   
+    }
+    else
+    {
+    
+        lcd.setCursor(0,0);
+      lcd.print("    STOP    ");
+    digitalWrite(M16, LOW);
+  digitalWrite(M21, LOW);  
+   
+    }
+  
+ 
+}
+
+
---
+++ transmit_task1/transmit_task1.ino
@@ -0,0 +1,71 @@
+ 
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int s1 = 10;
+int s2 = 8;
+int s3 = 9;
+int s4 = 7;
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(s1, INPUT_PULLUP);
+  pinMode(s2, INPUT_PULLUP);
+  pinMode(s3, INPUT_PULLUP);
+  pinMode(s4, INPUT_PULLUP);
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("WIRELESS");
+  lcd.setCursor(0, 1);
+  lcd.print("CAR");
+  delay(500);
+  lcd.clear();
+}
+
+void loop()
+{
+  int a = 1;
+  int b = 2;
+  int c = 3;
+  int d = 4;
+  int e = 20;
+  if(digitalRead(s1) == LOW)
+  {
+    Serial.write(a);
+   
+    lcd.setCursor(0, 0);
+    lcd.print("   FORWARD");
+  }
+  else if(digitalRead(s2) == LOW)
+  {
+    Serial.write(b);
+   
+    lcd.setCursor(0, 0);
+    lcd.print("  BACKWARD");
+  }
+  else if(digitalRead(s3) == LOW)
+  {
+    Serial.write(c);
+  
+    lcd.setCursor(0, 0);
+    lcd.print("RIGHT TURN");
+  }
+  else if(digitalRead(s4) == LOW)
+  {
+    Serial.write(d);
+   
+    lcd.setCursor(0, 0);
+    lcd.print("LEFT TURN");
+  }
+  else
+  {
+    
+    Serial.write(e);
+    lcd.print("    START     ");
+    lcd.clear();
+  }
+  
+}
